082bd56baf51e83df5ab73b757db48e01c7246a3
jingleville <andrewgavrick@yandex.ru>
2023-03-08 22:20:25 +0300

    added login

--- app.rb
+++ app.rb
@@ -14,7 +14,39 @@ post '/' do
   @title = 'Thank you!'
   @message = "Dear #{@user_name}, we'll wait gor you at #{@date_time}"
 
+  f = File.open 'users.txt', 'a' 
+  f.write "User #{@user_name}, phone #{@phone}, date is #{@date_time}"
+  f.close
+
   erb :welcome
+end
+
+get '/admin' do
+  if @login != 'admin'
+    return erb :login
+  end
+
+  if @password != 'secret'
+    return erb :login
+  end
+
+  erb :admin
+end
+
+post '/admin' do
+  @login = params[:login]
+  @password = params[:password]
+
+  if @login != 'admin'
+    return erb :login
+  end
+
+  if @password != 'secret'
+    return erb :login
+  end
+
+  @users_list = File.read 'users.txt'
+  erb :admin
 end
 
 #almost finished lesson20
